fe7233c9ba9569a0ccbd36e522abd055b4bc211f
Everett <everettmay1025@gmail.com>
2024-10-07 12:21:27 -0400

    1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,22 @@
-require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-ignition-ethers");
+require("@nomicfoundation/hardhat-toolbox");
+
+// this is from the tutorial https://hardhat.org/tutorial/deploying-to-a-live-network
+// see that URL for explanations.
+
+const { vars } = require("hardhat/config");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+
 module.exports = {
   solidity: "0.8.27",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
-
-require("@nomicfoundation/hardhat-toolbox");
